320c245e02d1ee74be2a25cb7e5e7fe29782dea3
Jinky Lopez <jinky.a.lopez@accenture.com>
2026-05-21 10:58:42 +0000

    added mock server

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,10 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Sorter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+
+], function (Controller, Sorter, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("sapips.training.odata.controller.Main", {
-        onInit() {
+        onInit: function () {
+            var oTable = this.byId("tabProduct");
+            var oBinding = oTable.getBinding("items");
+
         }
     });
 });
